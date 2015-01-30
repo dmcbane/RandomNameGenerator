f22acf8c68bc5a738e5b90821c1bbd6f77d07e2a
Emery A Miller <emerybmiller@gmail.com>
2015-01-30 21:24:47 +0000

    Update version and add a generator method

--- lib/random_word_generator.rb
+++ lib/random_word_generator.rb
@@ -2,6 +2,7 @@ require "random_word_generator/version"
 require "random_word_generator/random_name_generator"
 
 module RandomWordGenerator
+  SAMPLE_FILE = File.expand_path('../media/greek_myth_sample', __dir__)
   def self.fix_sample(input_file)
     names = File.read(input_file).split
     File.open(input_file, 'w') do |f|
@@ -10,9 +11,13 @@ module RandomWordGenerator
     end
   end
 
-  def self.generate(count: 1, base_file: File.expand_path('../media/greek_myth_sample', __dir__), &process_name)
+  def self.generate(count = 1, base_file: SAMPLE_FILE, &process_name)
     process_name ||= proc {|name| puts name}
     generator = RandomNameGenerator.new(base_file)
     count.times { process_name.call(generator.generate) }
+  end
+
+  def self.generator(base_file: SAMPLE_FILE)
+    RandomNameGenerator.new(base_file)
   end
 end
