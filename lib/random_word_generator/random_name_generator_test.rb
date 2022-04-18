b59da7010a9663fb3bd0bc42045dbe05997dfb0b
H. Dale McBane <h.dale.mcbane@gmail.com>
2022-04-18 12:34:52 -0400

    with generator, you can dump probability tables as json

--- lib/random_word_generator/random_name_generator_test.rb
+++ lib/random_word_generator/random_name_generator_test.rb
@@ -1,6 +1,6 @@
 require_relative 'random_name_generator'
 
-generator = RandomNameGenerator.new("media/greek_myth_sample")
+generator = RandomNameGenerator.new("media/places_sample")
 
 puts "Generating 40 names"
 40.times {puts generator.generate}
